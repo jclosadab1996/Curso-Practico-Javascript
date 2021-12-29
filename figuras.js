171f928416b46681bb73a2454cbc20c318dbfa54
Juan Camilo Losada <jclosadab1996@javerianacali.edu.co>
2021-12-28 20:15:36 -0500

    Commit 3

--- figuras.js
+++ figuras.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+//Codigo del cuadrado
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + " Cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + " Cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado + " Cm^2");
+
+//Codigo del triangulo
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+console.log("Los lados del triangulo miden: " + ladoTriangulo1 + "cm, " + ladoTriangulo2 + "cm, " + baseTriangulo + "cm");
